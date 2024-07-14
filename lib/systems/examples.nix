jcaesar/fork-nixpkgs

rustc: set up sysroot for wasm32-wasi and fix build

lib/systems/examples.nix
@@ -353,6 +353,7 @@ rec {
 
   wasi32 = {
     config = "wasm32-unknown-wasi";
+    rustc.config = "wasm32-wasi";
     useLLVM = true;
   };
 

jcaesar/fork2pr-nixpkgs

lib/systems/examples.nix
@@ -353,6 +353,7 @@ rec {
 
   wasi32 = {
     config = "wasm32-unknown-wasi";
+    rustc.config = "wasm32-wasip1";
     useLLVM = true;
   };
 